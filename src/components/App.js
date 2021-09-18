24fe1008974b597fb2a95dbf4c08f7574a67aa26
PremanandChowdhury <freudigprem@gmail.com>
2021-09-18 22:21:02 +0530

    code: Used axios to fetch data from unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import axios from 'axios'
 import '../index.css'
 
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term)
+  const onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID sH1dHMPScortciWomIO6561h1-J8AWCGT-zCjgz4k7w',
+      },
+    })
+    console.log(response.data.results)
   }
   return (
     <div className='container'>
